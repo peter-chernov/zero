d8cdde2bfe132d61150da030d948dd13456820d2
petr-chernov <44315626+petr-chernov@users.noreply.github.com>
2021-12-04 19:15:21 +0300

    123
    123

--- index.js
+++ index.js
@@ -1,3 +1,2 @@
 #!/usr/bin/env node
-console.log( "Правила использования искуственного интелекта ZERO:" );
-console.log( "1) Не использовать в " );
+console.log( "MB2100series_3DAFE8000000 webOS_TV_43UN68006LA LDPC_Sergey__Chernov_(DESCTOP-O60CMKL)" );
