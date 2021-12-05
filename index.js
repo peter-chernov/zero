ed1164ed956a137b48b56483aaedd3a852f3cfa8
petr-chernov <44315626+petr-chernov@users.noreply.github.com>
2021-12-05 08:37:09 +0300

    ;
    ;

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 #!/usr/bin/env node
-let message = "Hello, ";
-let name = process.argv[2];
-if (name == undefined) {
-  name = "";
+  let message = "Hello ";
+  let message2 = ', how are you';
+  let name2 = process.argv[3];
+  let name3 = process.argv[2];
+  if (name3 === 'hi' || name3 === 'hello') {
+    if (name2 === undefined) {
+      name2 = "";
+      message2 = 'how are you';
+      message = 'Hello, '
+    }
+    message = message + name2 + message2;
+    delete name2;
+    console.log( message );
 }
-message = message + name;
-console.log( message );
